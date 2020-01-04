c9afddf79612cdb6d415c385da40c4d8ebfc35ae
RefundGarbage <stevenxu1202@yahoo.com>
2020-01-04 00:07:40 -0800

    Added Google Firebase Connection Rebuilt project Currently building without problems

--- app/src/main/java/org/refundgarbage/uraniumforum/MainActivity.java
+++ app/src/main/java/org/refundgarbage/uraniumforum/MainActivity.java
@@ -1,7 +1,9 @@
 package org.refundgarbage.uraniumforum;
 
 import android.os.Bundle;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
--- app/src/main/java/org/refundgarbage/uraniumforum/ui/home/HomeFragment.java
+++ app/src/main/java/org/refundgarbage/uraniumforum/ui/home/HomeFragment.java
@@ -5,11 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
 import org.refundgarbage.uraniumforum.R;
 
 public class HomeFragment extends Fragment {
@@ -17,7 +19,7 @@ public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
